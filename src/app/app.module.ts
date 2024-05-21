b206a9405541999b5d12eb9c0ec967f6afac7f73
cesar.vidalmnd@uanl.edu.mx <cesar.vidalmnd@uanl.edu.mx>
2024-05-21 17:19:34 -0600

    Integracion de Firebase y creacion del servicio user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"recordatorio-a6a35","appId":"1:436168261401:web:4859275f44113759b30f88","storageBucket":"recordatorio-a6a35.appspot.com","apiKey":"AIzaSyDGfOmhedGCAv5UODJPzDLEaubKZhv1J-8","authDomain":"recordatorio-a6a35.firebaseapp.com","messagingSenderId":"436168261401"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
   screen: any = 'signin';
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit(): void {
   }
 
   change(event: any){
