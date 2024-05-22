2efe857801406e8908ee359bf110a8bb2ed24e87
cesar.vidalmnd@uanl.edu.mx <cesar.vidalmnd@uanl.edu.mx>
2024-05-22 13:12:04 -0600

    Registro de usuario y login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +11,43 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
   screen: any = 'signin';
+  email: string = '';
+  password: string = '';
+  
 
-  constructor() {}
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
   change(event: any){
     this.screen = event;
+  }
+
+  onSubmit() {
+    const formData = {
+      email: this.email,
+      password: this.password
+    };
+
+
+    if(this.screen == "signup")
+      this.userService.register(formData)
+        .then(response => {
+          console.log(response);
+          this.router.navigate(['/login']);
+          this.screen = 'signin'
+        })
+        .catch(error => console.log(error));
+    else if(this.screen =="signin")
+      this.userService.login(formData)
+        .then(response => {
+          console.log(response);
+          this.router.navigate(['/config']);
+        })
+        .catch(error => console.log(error));  
   }
 }
