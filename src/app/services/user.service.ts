fd42789fb5e8807acea9bddffd329a435d4db504
Rachmiel368 <rachmiel123_@outlook.es>
2024-05-24 14:10:51 -0600

    Se agregaron Toast para alertar errores de Login y Signup

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '@angular/fire/auth';
-
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private toastController: ToastController) { }
 
   register({email, password}: any){
     return createUserWithEmailAndPassword(this.auth, email, password);
@@ -17,4 +17,12 @@ export class UserService {
     return signInWithEmailAndPassword(this.auth,email,password)
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      position: 'bottom',
+    });
+    await toast.present();
+  }
 }
