9a18782133d5e376aa12f0774d8f34cb78d3207b
AngelF4 <angelfhernandez71@gmail.com>
2024-05-24 12:24:29 -0600

    Se agrego el modal a los detalles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { CheckboxCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +14,20 @@ export class HomePage {
   repeatArray = Array(15).fill(0)
   constructor(private router: Router, private popoverCtrl: PopoverController) {}
 
+  canDismiss = false;
+  presentingElement: any;
+
   segmentValue:String = 'lista';
   isOpen = false;
+
+  ngOnInit() {
+    this.presentingElement = document.querySelector('ion-content');
+  }
+
+  onTermsChanged(event: Event) {
+    const ev = event as CheckboxCustomEvent;
+    this.canDismiss = ev.detail.checked;
+  }
 
   segmentChanged(event: any) {
     this.segmentValue = event.detail.value;
