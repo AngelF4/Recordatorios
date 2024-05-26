4a7467112f1ccd615f8592a2261880157e03d5d3
Cesarw-Vidal <cesar.vidalmnd@uanl.edu.mx>
2024-05-26 01:38:22 -0600

    obteniendo highlightedDates para mostrar en calendario

--- src/app/interfaces/recordatorio.ts
+++ src/app/interfaces/recordatorio.ts
@@ -6,3 +6,9 @@ export interface Recordatorio {
     notes: string,
     date: Timestamp
 }
+
+export interface HighlightedDate {
+    date: string;
+    textColor: string;
+    backgroundColor: string;
+  }
